38634b18c8f6dbf61c1e2ba3d8e2cd2be742cc3c
notMago <72149359+notMago@users.noreply.github.com>
2020-11-04 12:58:41 -0300

    Initial commit

---
+++ src/pages/composicion.js
@@ -0,0 +1,16 @@
+import { Box } from "@chakra-ui/core";
+import { Navigation } from "../components/Navigation";
+
+const ComposicionPage = () => {
+  return (
+    <>
+      <Navigation>
+        <Box padding="5px" alignSelf="center">
+          Pagina Composicion
+        </Box>
+      </Navigation>
+    </>
+  );
+};
+
+export default ComposicionPage;
